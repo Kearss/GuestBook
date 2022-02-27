f5321660ee1eda1e7684b44bd0988a249d86cfa8
Kearsz <16836684+Kearsz@users.noreply.github.com>
2022-02-27 16:30:57 +0200

    nice nice
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,15 +1,70 @@
 var express = require("express");
+var fs = require("fs");
 var app = express();
+
+var bodyParser =require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function (req, res){
     res.sendFile(__dirname +'/index.html');
 });
+app.post('/', function (req, res){
+    var data = require('./Dataset.json')
+    data.push({
+        "Name": req.body.Name,
+        "User": req.body.User,
+        "Message":req.body.Message,
+        "Date": new Date()
+        });
+    var jsonStr = JSON.stringify(data);
+
+    fs.writeFile('Dataset.json', jsonStr,(err)=>{
+        if(err) throw err;
+        console.log('it\'s saved!');
+    });
+    res.send("Saved the data to a file. Browse to the /guestbook to see the contents of the file");
+    });
+
 app.get('/guestbook', function (req, res){
-    res.send('Guest!');
+    var data = require('./Dataset.json')
+    var result= '<table border ="1">';
+    for (var i=0; i< data.length; i++){
+        result +=
+        '<tr>'+
+        '<td>'+data[i].Name+'</td>'+
+        '<td>'+data[i].User+'</td>'+
+        '<td>'+data[i].Message+'</td>'+
+        '</tr>';
+    }
+    res.send(result);
+    
 });
+
 app.get('/newmessage', function (req, res){
-    res.send('Forum!');
+    var data = require('./Dataset.json')
+
+    var result= '<table border ="1">';
+    for (var i=0; i< data.length; i++){
+        result +=
+        '<tr>'+
+        '<td>'+data[i].Name+'</td>'+
+        '<td>'+data[i].Email+'</td>'+
+        '</tr>';
+    }
+    res.send(result);
+
+
+    var jsonStr = JSON.stringify(data);
+
+    fs.writeFile('Dataset.json', jsonStr,(err)=>{
+        if(err) throw err;
+        console.log('it\'s saved!');
+    });
+    res.send("Saved the data to a file. Browse to the /details to see the contents of the file");
+
 });
+
+
 app.get('/ajaxmessage', function (req, res){
     res.send('something dont know!');
 });
