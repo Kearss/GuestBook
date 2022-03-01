eac435edfc9657babaf71cc5179eae5f2df3d99c
Kearsz <16836684+Kearsz@users.noreply.github.com>
2022-03-01 11:43:25 +0200

    new stuff
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,72 +2,54 @@ var express = require("express");
 var fs = require("fs");
 var app = express();
 
-var bodyParser =require('body-parser');
+var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function (req, res){
     res.sendFile(__dirname +'/index.html');
 });
-app.post('/', function (req, res){
+
+app.get('/guestbook', function (req, res){
     var data = require('./Dataset.json')
+    var result= '<table border ="1"><tr><th>username</th><th>Country</th><th>Message</th></tr>';
+    for (var i=0; i< data.length; i++){
+        result +=
+        '<tr>'+
+        '<td>'+data[i].Username +'</td>'+
+        '<td>'+data[i].Country+'</td>'+
+        '<td>'+data[i].Message+'</td>'+
+        '</tr>';
+    }
+    res.send(result); 
+});
+app.get('/newmessage', function (req, res){
+    res.sendFile(__dirname +'/message.html');
+});
+
+app.post('/newmessage', function (req, res){
+
+    var data = require('./Dataset.json')
+    // creates a new Json object and adds it to a existing data variable
     data.push({
-        "Name": req.body.Name,
-        "User": req.body.User,
+        "Username": req.body.Username,
+        "Country": req.body.Country,
         "Message":req.body.Message,
         "Date": new Date()
         });
+    //converts Json in to string format 
     var jsonStr = JSON.stringify(data);
-
+    //writes fata to file
     fs.writeFile('Dataset.json', jsonStr,(err)=>{
         if(err) throw err;
         console.log('it\'s saved!');
     });
     res.send("Saved the data to a file. Browse to the /guestbook to see the contents of the file");
-    });
-
-app.get('/guestbook', function (req, res){
-    var data = require('./Dataset.json')
-    var result= '<table border ="1">';
-    for (var i=0; i< data.length; i++){
-        result +=
-        '<tr>'+
-        '<td>'+data[i].Name+'</td>'+
-        '<td>'+data[i].User+'</td>'+
-        '</tr>';
-    }
-    res.send(result);
-    
 });
-
-app.get('/newmessage', function (req, res){
-    var data = require('./Dataset.json')
-
-    var result= '<table border ="1">';
-    for (var i=0; i< data.length; i++){
-        result +=
-        '<tr>'+
-        '<td>'+data[i].Name+'</td>'+
-        '<td>'+data[i].Email+'</td>'+
-        '</tr>';
-    }
-    res.send(result);
-
-
-    var jsonStr = JSON.stringify(data);
-
-    fs.writeFile('Dataset.json', jsonStr,(err)=>{
-        if(err) throw err;
-        console.log('it\'s saved!');
-    });
-    res.send("Saved the data to a file. Browse to the /details to see the contents of the file");
-
-});
-
 
 app.get('/ajaxmessage', function (req, res){
-    res.send('something dont know!');
+    res.sendFile(__dirname +'/ajax.html');
 });
 
 app.listen(8081, function(){
-    console.log('Example app listetning on port 8081!');
+    console.log('Example app listening on port 8081!');
 });
