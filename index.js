6e5c6b60842cf1062a5125d4697de964c299fe6b
Kearsz <16836684+Kearsz@users.noreply.github.com>
2022-03-01 20:38:36 +0200

    many many changes
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
+// Määritellään palvelimelle portti.
 const PORT = process.env.PORT || 8081;
-var express = require("express");
-var fs = require("fs");
-var app = express();
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:true}));
+// Otetaan moduuleja käyttöön.
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var bodyParser = require("body-parser");
+
+// Otetaan body-parser käyttöön express-sovelluksessa.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Sisältö haetaan public-hakemistosta.
+app.use(express.static("./publicview"));
 
 app.get('/', function (req, res){
     res.sendFile(__dirname +'/index.html');
 });
 
 app.get('/guestbook', function (req, res){
-    var data = require('./Dataset.json')
-    res.sendFile(__dirname +'/guest.html');
+    res.sendFile(__dirname +'/publicview/guest.html');
 });
 
 app.get('/newmessage', function (req, res){
-    res.sendFile(__dirname +'/message.html');
+    res.sendFile(__dirname +'/publicview/message.html');
 });
 
 app.post('/newmessage', function (req, res){
 
-    var data = require('./Dataset.json')
+    var data = require('./publicview/Dataset.json')
     // creates a new Json object and adds it to a existing data variable
     data.push({
         "Username": req.body.Username,
@@ -40,10 +46,10 @@ app.post('/newmessage', function (req, res){
 });
 
 app.get('/ajaxmessage', function (req, res){
-    res.sendFile(__dirname +'/ajax.html');
+    res.sendFile(__dirname +'/publicview/ajax.html');
 });
 
 
 app.listen(PORT, function(){
-    console.log('Example app listening on port 8081!' + PORT);
+    console.log('Example app listening on port!' + PORT);
 });
