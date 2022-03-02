9f4be7cdc36ce75a7c7f628df8e091aa6f17952d
Kearsz <16836684+Kearsz@users.noreply.github.com>
2022-03-02 10:05:45 +0200

    heroku fix?
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -13,7 +13,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Sisältö haetaan public-hakemistosta.
 app.use(express.static("./public"));
 
+// Lisätään dataa olemassaolevaan JSON tiedostoon.
+app.post("/kirjaudu", function (req, res) {
+    // Ladataan dataa JSON tiedostosta muuttujaan.
+    var data = require("./public/data.json");
+    // Määritellään päivämäärän haku.
+    var today = new Date();
+    var date = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
+    // Luodaan JSON objekti ja lisätään se data-muuttujaan.
+    data.push({
+        "Email": req.body.email,
+        "Salasana": req.body.pass,
+        "Date": date
+    });
+    // Muunnetaan JSON objekti tekstimuotoon.
+    var jsonStr = JSON.stringify(data);
+    // Kirjoitetaan data JSON tiedostoon.
+    fs.writeFile("public/data.json", jsonStr, (err) => {
+        if (err) throw err;
+        console.log("...It is saved!");
+    });
+    // Esitetään haluttu data.
+    res.send("You send an AJAX message: " + message + ". It is saved to a JSON file");
+});
 
+
+// Luodaan web-palvelin.
 app.listen(PORT, () => {
     console.log("Example app listening on port " + PORT);
 });
