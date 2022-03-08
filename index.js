7077242b21034e4cb236d1a47a1b2e090f174296
Kearsz <16836684+Kearsz@users.noreply.github.com>
2022-03-08 12:32:08 +0200

    changing to ejs
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,68 +1,84 @@
-// Määritellään palvelimelle portti.
-const PORT = process.env.PORT || 8000;
-
-// Otetaan moduuleja käyttöön.
-const guests = require("./dataset.json");
+const guests = require("./guests.json");
 const fs = require("fs");
 const express = require("express");
 const app = express();
 
-
-// Otetaan body-parser käyttöön express-sovelluksessa.
+//For handling the form
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', function (req, res){
-    res.render("index.html");
+//Including EJS
+app.set("view engine", "ejs");
+
+// Getting the right port dynamically
+const PORT = process.env.PORT || 8000;
+
+// Home Page
+app.get("/", (req, res) => {
+  res.render("pages/index.ejs");
 });
 
-app.get('/guestbook', function (req, res){
-    const table = makeTable;
-    res.render('guest', {table: table}, function (err, html) {
-        res.send(html)
-      })
-});
-app.get('/newmessage', function (req, res){
-    res.render(__dirname +'/message.html');
+// Guestbook Page
+app.get("/guestbook", (req, res) => {
+  const table = makeTable;
+  res.render("pages/guestbook", {table: table});
 });
 
-app.post('/addNewMessage', function (req, res){
-    addNewGuest(req.body.name, req.body.country, req.body.message);
-    res.redirect("/guestbook.html");
+// New Message Page
+app.get("/newmessage", (req, res) => {
+  res.render("pages/newmessage");
 });
-// Luodaan web-palvelin.
+
+// New Ajax Message Page
+app.get("/ajaxmessage", (req, res) => {
+  res.render("pages/ajaxmessage");
+});
+
+// POST route for Ajax Message Page
+app.post("/addAjaxMessage", (req, res) => {
+  addNewGuest(req.body.name, req.body.country, req.body.message);
+  res.send(makeTable());
+})
+
+// POST route for New Message Page
+app.post("/addNewMessage", (req, res) => {
+  addNewGuest(req.body.name, req.body.country, req.body.message);
+  res.redirect("/guestbook");
+})
+
+//Starting the server
 app.listen(PORT, () => {
-    console.log("app listening on port " + PORT);
-});
+  console.log("App is running on port 8000");
+})
 
 // Helper function for making the HTML-Table out of Guests JSON-data
 function makeTable() {
-    const guests = require("./dataset.json");
-    const guestsFormat = guests.map(guest => (
-      `<tr><td class="tohide">${guest.id}</td><td>${guest.username}</td><td>${guest.country}</td><td class="tohide">${guest.date}</td><td>${guest.message}</td></tr>`
-    ))
-    .reduce((prevValue, curValue) => prevValue + curValue);
-  
-    return (`<table class="table"><thead class="thead-dark"><tr><th class="tohide">ID</td><th>Name</th><th>Country</th><th class="tohide">Date</th><th>Message</th></tr></thead><tbody>
-    ${guestsFormat}
-    </tbody></table>`);
-  }
+  const guests = require("./guests.json");
+  const guestsFormat = guests.map(guest => (
+    `<tr><td class="tohide">${guest.id}</td><td>${guest.username}</td><td>${guest.country}</td><td class="tohide">${guest.date}</td><td>${guest.message}</td></tr>`
+  ))
+  .reduce((prevValue, curValue) => prevValue + curValue);
 
-  //Helper function to add a new Guest to local variable and JSON file
+  return (`<table class="table"><thead class="thead-dark"><tr><th class="tohide">ID</td><th>Name</th><th>Country</th><th class="tohide">Date</th><th>Message</th></tr></thead><tbody>
+  ${guestsFormat}
+  </tbody></table>`);
+}
+
+//Helper function to add a new Guest to local variable and JSON file
 function addNewGuest(username, country, message) {
-    const newGuestObject = {
-      id: guests.length + 1,
-      username: username,
-      country: country,
-      date: Date(),
-      message: message
-    }
-    guests.push(newGuestObject);
-  
-    const guestsString = JSON.stringify(guests);
-  
-    fs.writeFile("guests.json", guestsString, (err) => {
-      if (err) throw err;
-      console.log("Guest has been saved!");
-    })
+  const newGuestObject = {
+    id: guests.length + 1,
+    username: username,
+    country: country,
+    date: Date(),
+    message: message
   }
+  guests.push(newGuestObject);
+
+  const guestsString = JSON.stringify(guests);
+
+  fs.writeFile("guests.json", guestsString, (err) => {
+    if (err) throw err;
+    console.log("Guest has been saved!");
+  })
+}
