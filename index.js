69029c44c31cc26b28a4e0bdccef82f0f5558465
Kearsz <16836684+Kearsz@users.noreply.github.com>
2022-03-08 10:45:40 +0200

    testing
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Including EJS
+app.set("view engine", "ejs");
+
 // Home Page
 app.get("/", (req, res) => {
     res.render("index.ejs");
