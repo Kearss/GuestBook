967899ec67ba8447b70edfb80215577ca9b07122
Kearsz <16836684+Kearsz@users.noreply.github.com>
2022-03-10 10:56:29 +0200

    adding date
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/guestbook", (req, res) => {
     var bootstrap = "<link rel="+'stylesheet'+" href=https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css></link>"
     var nav ="";
     let results = bootstrap+"<table class='table' style='width: 100%'> <thead class='thead-dark'>" +
-      '<tr><th>Name</th><th>Country</th><th>Message</th></tr>';
+      '<tr><th>Name</th><th>Country</th><th>Date</th><th>Message</th></tr>';
     for (let i = 0; i < data.length; i++) {
       results +=
         '<tr>' +
